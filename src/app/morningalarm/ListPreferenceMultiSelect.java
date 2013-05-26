a77cd18243761ad92c591a6a0a4c2b12ec629311
profira <profiralex@gmail.com>
2013-05-26 17:46:40 +0300

    start working on database

--- src/app/morningalarm/ListPreferenceMultiSelect.java
+++ src/app/morningalarm/ListPreferenceMultiSelect.java
@@ -12,7 +12,6 @@ import android.content.DialogInterface;
 import android.content.res.TypedArray;
 import android.preference.ListPreference;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ListView;
 
 /**
